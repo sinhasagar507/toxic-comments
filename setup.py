89ef9ab92192257f1201c09c06aa7d50dedd75c0
Sagar Sinha <sinhasagar507@gmail.com>
2023-11-27 18:00:22 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multilabel classification of toxic comments from the famous Common Crawl dataset ',
+    author='Sagar Sinha ',
+    license='MIT',
+)
